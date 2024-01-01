0b2589b163345b65f5ca8e850e52405977e31951
datager <daska110@126.com>
2024-01-01 19:57:40 +0800

    chore: rsvp.query sql function pass

--- migrations/20231226153854_reservation_func.up.sql
+++ migrations/20231226153854_reservation_func.up.sql
@@ -3,6 +3,7 @@
 -- if both are null, find all reservations within during
 -- if both set, find all reservations within during for the resource and user
 -- 这个函数，开发者可以决定放在应用程序层，或放在数据库层，都可以的，主要是看数据库是否容易实现，不必想太多
+-- select * from rsvp.query('y', NULL, '("2023-01-01", "2024-01-01")');
 CREATE OR REPLACE FUNCTION rsvp.query(
     uid TEXT, 
     rid TEXT, 
@@ -17,7 +18,8 @@ DECLARE
 BEGIN
     -- format the query based on parameters
     _sql := format(
-        'SELECT * FROM rsvp.reservations WHERE during @> timespan AND $1 ORDER BY LOWER(timespan) %s LIMIT %s OFFSET %s', 
+        'SELECT * FROM rsvp.reservations WHERE %L @> timespan AND %s ORDER BY LOWER(timespan) %s LIMIT %s OFFSET %s', 
+        during,
         CASE
             WHEN uid IS NULL AND rid IS NULL THEN 'TRUE'
             WHEN uid IS NULL THEN 'resource_id = ' || quote_literal(rid)
